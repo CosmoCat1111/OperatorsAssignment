5e8f9db49a25117b287f783126d5c9679ef5c108
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-18 10:34:12 -0700

    Add project files.

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        //properties of class
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Employee(int id, string firstName, string lastName) //constructor to initialize properties
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1?.Id == emp2?.Id; //check if equal
+        }
+
+
+        public static bool operator !=(Employee emp1, Employee emp2) //overload the opposite == and !=
+            {
+            return !(emp1 == emp2); //check if not equal
+            }
+    }
+}
---
+++ OperatorsAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(1, "Steve", "Jobs"); //creating employees
+            Employee emp2 = new Employee(2, "Fred", "Penner");
+
+            if (emp1 == emp2)
+            {
+                Console.WriteLine("Both Employees are the same!");
+            }
+            else
+            {
+                Console.WriteLine("Emp1 and Emp2 are not the same!");
+            }
+
+
+
+            Employee emp3 = new Employee(1, "Barbara", "Walters"); //create new emp to compare to emp1 
+            
+                if (emp1 == emp3) // comparing to see if same
+            {
+                Console.WriteLine("Emp1 and Emp3 are the same!");
+            }
+                else // comparing to see if not 
+            {
+                Console.WriteLine("Emp1 and Emp3 are not the same!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
